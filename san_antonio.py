610ce4286fdc79baad7159f205ff49aa05396d0b
pshop <choppin.p@gmail.com>
2017-10-30 16:32:18 +0100

    Structurez votre programme en utilisant les fonctions

--- san_antonio.py
+++ san_antonio.py
@@ -13,9 +13,21 @@ characters = [
     "Kirikou"
 ]
 
+user_answer = input('Appuyez sur enter pour une nouvelle citation ou \'B\' pour quitter')
+
 if user_answer == "B":
     pass
 elif user_answer == "C":
     print("C pas la bonne réponse ! Et G pas d’humour, je C...")
 else:
+    pass
     # show another quote
+
+def get_random_item_in(my_list):
+    # todo: get a random number
+    item = my_list[0] # get a quote from a list
+    print(item) # show the quote in the interpreter
+    return "program is over" # returned value
+
+
+print(get_random_item_in(quotes))
